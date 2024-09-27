a1bbc9ac1662dca6b81557e57d49fbbe1924f59e
20essentials <s0l0estudi4r@gmail.com>
2024-09-27 20:46:28 +0200

    v0.0.2

--- script.js
+++ script.js
@@ -1,56 +1,107 @@
 let d = document;
+const $ = (el) => d.querySelector(el);
+const $delete = $(".delete");
+const $copy = $(".copy");
+const $textarea = $("textarea");
+const $btnCapitalize = $(".to-capitalize");
+const $btnLowercase = $(".to-lowercase");
+const $btnUppercase = $(".to-Uppercase");
+const $btnAddUnderscores = $(".add-underscores");
+const $btnAddHyphens = $(".add-hyphens");
+const $btnRemoveUnderscores = $(".remove-underscores");
+const $btnRemoveHyphens = $(".remove-hyphens");
 
-d.addEventListener("click", e => {
-  let $textarea = d.querySelector("textarea");
-  let $textareaValue = d.querySelector("textarea").value.trim();
+const toCapitalize = (text) => {
+  return text
+    .split(' ')
+    .map(el => `${el[0].toUpperCase()}${el.slice(1)}`)
+    .join(' ');
+}
+const removeClasses = () => ($textarea.className = "");
+const addSymbol = (text, symbol) => {
+  return text
+    .split(" ")
+    .map((el) => `${el}${symbol}`)
+    .join("")
+    .slice(0, -1)
+    .trim();
+};
+const removeSymbol = (text, symbol) => {
+  return text.split(symbol).join(" ").trim();
+};
 
-  if ($textareaValue !== "") {
-    if (e.target.matches(".may")) {
-      $textarea.value = $textareaValue.toUpperCase();
-    }
+/* ====================== DELEGATION EVENTS ===================== */
 
-    if (e.target.matches(".min")) {
-      $textarea.value = $textareaValue.toLowerCase();
-    }
+d.addEventListener("click", (e) => {
+  if (e.target === $copy) {
+    $textarea.select();
+    $textarea.setSelectionRange(0, 5000000);
+    navigator.clipboard.writeText($textarea.value);
+    return;
+  }
+  if (e.target === $delete) {
+    $textarea.value = "";
+    return;
+  }
+  if (e.target === $btnCapitalize) {
+    removeClasses();
+    $textarea.value = toCapitalize($textarea.value);
+    return;
+  }
+  if (e.target === $btnLowercase) {
+    removeClasses();
+    $textarea.value = $textarea.value.toLowerCase().trim();
+    return;
+  }
+  if (e.target === $btnUppercase) {
+    removeClasses();
+    $textarea.value = $textarea.value.toUpperCase().trim();
+    return;
+  }
+  if (e.target === $btnAddUnderscores) {
+    $textarea.value = addSymbol($textarea.value, "_");
+    return;
+  }
 
-    if (e.target.matches(".cap")) {
-      let array = $textareaValue.toLowerCase().split(" "), nuevoArray = [];
-      array.forEach(element => {
-        let parteDerecha = element.substring(1);
-        nuevoArray.push(element.at(0).toUpperCase() + parteDerecha);
-      });
+  if (e.target === $btnAddHyphens) {
+    $textarea.value = addSymbol($textarea.value, "-");
+    return;
+  }
 
-      $textarea.value = nuevoArray.join(" ").trim();
-    }
-
-    if (e.target.matches(".hyp")) {
-      let $array = $textareaValue.split(" ");
-      $textarea.value = $array.join("_");
-    }
+  if (e.target === $btnRemoveUnderscores) {
+    $textarea.value = removeSymbol($textarea.value, "_");
+    return;
+  }
+  if (e.target === $btnRemoveHyphens) {
+    $textarea.value = removeSymbol($textarea.value, "-");
+    return;
   }
 });
+
+/* ============================== RAIN ============================= */
 
 d.addEventListener("DOMContentLoaded", rain);
 
 function rain() {
-  let amount = 100, i = 0;
+  let amount = 120,
+    i = 0;
 
-  while ( i < amount) {
+  while (i < amount) {
     let burbuja = d.createElement("i");
     burbuja.classList.add("burbuja");
 
     let size = Math.random() * 25 + 6,
-      posX = Math.floor(Math.random() * window.innerWidth),
+      posX = Math.floor(Math.random() * 100),
       duration = Math.random() * 6 + 1,
       delay = Math.random() * -20;
 
-    burbuja.style.left = posX + "px";
+    burbuja.style.left = posX + "vw";
     burbuja.style.width = size + "px";
     burbuja.style.height = size + "px";
     burbuja.style.animationDuration = duration + "s";
     burbuja.style.animationDelay = delay + "s";
 
-    d.body.appendChild(burbuja);
+    $(".bg").appendChild(burbuja);
     i++;
   }
 }
